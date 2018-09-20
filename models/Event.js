b6bac56fc7dc770cc17de9fb74e62f3ea7d627a6
hectorbliss <contacto@fixter.org>
2018-09-20 17:21:33 -0500

    owner added to model

--- models/Event.js
+++ models/Event.js
@@ -2,6 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const eventSchema = new Schema({
+    owner:{
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
     participants:[{
         type:Schema.Types.ObjectId,
         ref:'User'
